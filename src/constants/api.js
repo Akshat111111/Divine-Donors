d4d1b507f0b5851afba36eb9e2d96f7d69c576ca
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2024-06-16 22:40:22 +0530

    Add files via upload

---
+++ src/constants/api.js
@@ -0,0 +1,1 @@
+export const RAZORPAY_PAYMENT_KEY_ID = "rzp_live_pBWB8jeSKHFY3Y";
---
+++ src/constants/faqs.js
@@ -0,0 +1,22 @@
+export const faqs = [
+    {
+        question: ' Why DIVINE DONORS is required?',
+        answer: '  India has a significant population living below the poverty line. In india  poverty in rural and urban India and especially childrens, increasing drastically is a big challenge for people to get enough food for the living.'
+    },
+    {
+        question: 'How does DIVINE DONORS work?',
+        answer: ' The DIVINE DONORS is a Crowdfunding platform where alumni can find their school & sponsor their choice amount or gift books or  spiritual literature as gratitude for their schools. They can also share this Campaign with other alumni to quickly complete the target.'
+    },
+    {
+        question: 'What books will be gifted to the school?',
+        answer: 'Spiritual books like Ramayana, Mahabharata, Sriman Bhagavatam, and Bhagavad Gita will be gifted to the schools.'
+    },
+    {
+        question: 'What if you can not Complete your targeted amount?',
+        answer: 'After the set target date whatever amount is collected worth those amount of books will be gifted to the school.'
+    },
+    {
+        question: 'What if the school refuses to accept the books?',
+        answer: 'If some school has any concerns we shall try our best to address those concerns, if still, any particular school is not willing to take the books then they will be given to some other interested school. '
+    }
+];
